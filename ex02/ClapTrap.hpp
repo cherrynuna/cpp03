a720a6ec417b19d29af6b9424d77c7df71ca2656
cherrynuna <inyoungstar7@gmail.com>
2024-11-13 14:27:41 +0900

    02

--- ex02/ClapTrap.hpp
+++ ex02/ClapTrap.hpp
@@ -17,19 +17,10 @@ class ClapTrap
 		ClapTrap&	operator=(const ClapTrap& fixed);
 		virtual ~ClapTrap();//가상 함수, 자식 함수에서 변환 했음을 표시+부모~자식소멸자
 
-		virtual void	attack(const std::string& target);
+		virtual void	attack(const std::string& target);// to call the derived class’s overridden version
 		void			takeDamage(unsigned int amount);
 		void			beRepaired(unsigned int amount);
-
-		// std::string		getName() const;
-		// unsigned int	getHitpoint(void) const;
-		// unsigned int	getEnergypoint(void) const;
-		// unsigned int	getAttackdamage(void) const;
-
-		// void	setHitpoints(unsigned int const num);
-		// void	setEnergypoints(unsigned int const num);
-		// void	setAttackdamage(unsigned int const num);
-
+		//virtual 키워드가 없으면 정적 바인딩이 이루어져서 컴파일 타임에 호출될 함수가 고정되기 때문에, 부모 클래스의 함수가 호출됩니다.
 };
 
 #endif
